c0898fd5a79e12711ca0ab686d31ae80bb717339
srujan703 <srujankumar_lolam@epam.com>
2025-01-09 12:13:47 +0530

    4 - Talk is cheap,show me the code

--- video3/notes3.js
+++ video3/notes3.js
@@ -33,3 +33,16 @@
 //config driven UI **
 //Based on the data received from the backed or API ,the UI get changes(for example,swiggy UI is differenct)
 //for different location based on the offers.This is known as config driven UI
+
+//FOr building large scale applications,all the static data is put on CDN to ensure smooth and faster user experience
+
+//always writing res.data.name,res.data.Time etc is not a good way of writing.It is not a clean code.Instead
+// destructure the objects(refer code) and use it
+
+//WHY KEY
+//whenever we loop over a list,always use Key attribute to uniquely identify every child element.
+//if we don't use key attribute,suppose if there are any additional child elemendts(restaurents in out case) to be added
+//then react cleans up and re renders all the child components from the start(restaurents in our case) as it could not
+//identify changes made to them(restaurents addition - in our case).If we use key,only the added component (restaurent
+//  in our case will render).This is an optimization technique
+// NOTE : Never index as keys ((restaurent,index) => {...})
