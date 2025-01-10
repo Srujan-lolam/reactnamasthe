5d6bfaed2a208a1fc242e9bbd93a7136ddabcf42
srujan703 <srujankumar_lolam@epam.com>
2025-01-10 21:27:23 +0530

    Episode5-Let'sget hooked version 2

--- Episode5/notes3.js
+++ Episode5/notes3.js
@@ -1,3 +1,25 @@
+//This video .episode is about restructuring our code by coding principles(standard ways) and learning hooks
+
+//Everything than is done using react can also be done using normal html,css and javascript.The only use is
+//it makes our job easy by providing some build in features
+
+//React does not have any official or recommended way for folder structure.It is not mandatory to seperate every file
+//from each other.It's the developer choice .But ,in ,reality an application will have 1000's of components.
+//So,maintaining a specific folder structure is better
+
+//It is again developer choice to use js extension of jsx extension.Hardly there is a slight difference.Jsx ,at the end
+//is a javascript code.Don't overhtink on it..keeping js extension keeps the code simple,easy to maintain
+
+//Never ever keep a hardcoded data or mock data in any component-it is not industry standard
+
+//Explained about Named export and default export - 2 ways (you know)
+
+//When using filtering feature for top rated components,using handleClick and filter,the restaurents are getting filtered
+//but the UI is not updating.That is where react comes into picture(hooks).
+
+// HOOK
+// It is a normal javascript utitlity function giving by react with some supernatural powers.most usefull hooks are
+// useState and useEffect
 //before building any application,we should plan it before(like how the UI looks like,what are the features etc),
 // so build wireframe(how it looks like)
 //for example for food ordering application we can have the following - Low level component
@@ -33,16 +55,3 @@
 //config driven UI **
 //Based on the data received from the backed or API ,the UI get changes(for example,swiggy UI is differenct)
 //for different location based on the offers.This is known as config driven UI
-
-//FOr building large scale applications,all the static data is put on CDN to ensure smooth and faster user experience
-
-//always writing res.data.name,res.data.Time etc is not a good way of writing.It is not a clean code.Instead
-// destructure the objects(refer code) and use it
-
-//WHY KEY
-//whenever we loop over a list,always use Key attribute to uniquely identify every child element.
-//if we don't use key attribute,suppose if there are any additional child elemendts(restaurents in out case) to be added
-//then react cleans up and re renders all the child components from the start(restaurents in our case) as it could not
-//identify changes made to them(restaurents addition - in our case).If we use key,only the added component (restaurent
-//  in our case will render).This is an optimization technique
-// NOTE : Never index as keys ((restaurent,index) => {...})
