1fa9ea0e1290e749becbf1dc9097dd3a77d0c658
Srujan <srujanlolam63@gmail.com>
2025-01-10 22:22:23 +0530

    Episode5-Let's get hooked final version

--- Episode5/src/components/Body.js
+++ Episode5/src/components/Body.js
@@ -1,41 +1,46 @@
 import RestaurentCard from "./RestaurentCard";
-let listOfRestaurents = [
-  {
-    id: 1,
-    name: "Pizza Palace",
-    rating: 3.5,
-    menu: [
-      { name: "Margherita Pizza", price: 12.99 },
-      { name: "Pepperoni Pizza", price: 14.99 },
-      { name: "Spaghetti Bolognese", price: 16.99 },
-    ],
-  },
-  {
-    id: 2,
-    name: "Dominos",
-    rating: 5,
-    menu: [
-      { name: "Margherita Pizza", price: 12.99 },
-      { name: "Pepperoni Pizza", price: 14.99 },
-      { name: "Spaghetti Bolognese", price: 16.99 },
-    ],
-  },
-  {
-    id: 3,
-    name: "KFC",
-    rating: 4,
-    menu: [
-      { name: "Margherita Pizza", price: 12.99 },
-      { name: "Pepperoni Pizza", price: 14.99 },
-      { name: "Spaghetti Bolognese", price: 16.99 },
-    ],
-  },
-];
-const handleClick = () => {
-  listOfRestaurents = listOfRestaurents.filter((x) => x.rating >= 4);
-  console.log(listOfRestaurents);
-};
+import { useState } from "react";
+import { restaurantsList } from "../../utils/mockData";
 const Body = () => {
+  const handleClick = () => {
+    const filteredListOfRestaurents = listOfRestaurents.filter(
+      (x) => x.rating >= 4.5
+    );
+    setListOfRestaurents(filteredListOfRestaurents);
+  };
+  const [listOfRestaurents, setListOfRestaurents] = useState(restaurantsList);
+  // const [listOfRestaurents, setListOfRestaurents] = useState([
+  //   {
+  //     id: 1,
+  //     name: "Pizza Palace",
+  //     rating: 3.5,
+  //     menu: [
+  //       { name: "Margherita Pizza", price: 12.99 },
+  //       { name: "Pepperoni Pizza", price: 14.99 },
+  //       { name: "Spaghetti Bolognese", price: 16.99 },
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Dominos",
+  //     rating: 5,
+  //     menu: [
+  //       { name: "Margherita Pizza", price: 12.99 },
+  //       { name: "Pepperoni Pizza", price: 14.99 },
+  //       { name: "Spaghetti Bolognese", price: 16.99 },
+  //     ],
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "KFC",
+  //     rating: 4,
+  //     menu: [
+  //       { name: "Margherita Pizza", price: 12.99 },
+  //       { name: "Pepperoni Pizza", price: 14.99 },
+  //       { name: "Spaghetti Bolognese", price: 16.99 },
+  //     ],
+  //   },
+  // ]);
   return (
     <div className="body">
       <div className="filter">
@@ -44,9 +49,10 @@ const Body = () => {
         </button>
       </div>
       <div className="restaurent-container">
-        {listOfRestaurents.map((res) => (
-          <RestaurentCard key={res.id} res1={res} />
-        ))}
+        {listOfRestaurents &&
+          listOfRestaurents.map((res) => (
+            <RestaurentCard key={res.id} res1={res} />
+          ))}
       </div>
     </div>
   );
